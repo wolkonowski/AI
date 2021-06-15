c5cefc6e13eddcbace38e289ac2ea0e13e7751dd
Krzysztof <k.wolkonowski.solutions@gmail.com>
2021-06-15 22:49:03 +0200

    Images added

--- loader.py
+++ loader.py
@@ -38,7 +38,8 @@ def load_data():
     below.
     """
     f = gzip.open('data/mnist.pkl.gz', 'rb')
-    training_data, validation_data, test_data = pickle.load(f, encoding="latin1")
+    training_data, validation_data, test_data = pickle.load(
+        f, encoding="latin1")
     f.close()
     return (training_data, validation_data, test_data)
 
--- test5.py
+++ test5.py
@@ -2,13 +2,21 @@ from f5 import *
 from N5 import Network
 import pickle
 import cv2 as cv
+import glob
+import numpy as np
 stream = open('data.pickle', 'rb')
 p: Network = pickle.load(stream, encoding='bytes')
 stream.close()
-file = 'own.png'
-image = cv.imread(file, cv.IMREAD_GRAYSCALE)
-image = 255-image
-image = image / 255.0
-image = image.reshape(784)
-s = p.start(image)
-print(np.argmax(s))
+files = glob.glob('images/*.png')
+total = len(files)
+num = 0
+for file in files:
+    image = cv.imread(file, cv.IMREAD_GRAYSCALE)
+    image = 255-image
+    image = image / 255.0
+    image = image.reshape(784)
+    s = p.start(image)
+    print(file, "result:", np.argmax(s))
+    if(int(file[-6]) == np.argmax(s)):
+        num += 1
+print(f"{num}/{total}")
